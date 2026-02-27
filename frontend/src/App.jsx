a649cf02c820c9ce02f06f197ecfae99aa2cec21
Pharadonw <pharadonw65@nu.ac.th>
2026-02-27 10:43:38 +0700

    Final27-1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,9 @@
-// src/App.jsx
 import Header from "./components/Header.jsx";
 import Navbar from "./components/Navbar.jsx";
 import Dashboard from "./components/Dashboard.jsx";
 import List from "./components/List.jsx";
-import Refer from "./components/Refer.jsx"; 
 import { Routes, Route } from "react-router-dom";
 import Admit from "./components/Admit.jsx";
-import Charts2 from "./components/Charts2.jsx";
-import Charts1 from "./components/Charts1.jsx";
 
 export default function App() {
   return (
@@ -18,11 +14,8 @@ export default function App() {
         <Route path="/" element={<Dashboard />} />
         <Route path="/patient" element={<List />} />
         <Route path="/Admitcase" element={<Admit/>} />        
-        <Route path="/Refercase" element={<Refer/>} />
         <Route path="*" element={<div style={{padding:16}}>Not Found</div>} />
       </Routes>
-      <Charts1/>
-      <Charts2/>
     </>
   );
 }
